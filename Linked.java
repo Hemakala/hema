838b0f3ffa57d15e2f9a384b9a6af41f8b87026a
Hemakala <hemakala1995@gmail.com>
2016-11-17 15:30:00 +0530

    Add files via upload

---
+++ Linked.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Linked
+{
+public static void main(String args[])
+{
+LinkedList<String> s=new LinkedList<String>();
+s.add("H");
+s.add("E");
+s.add("M");
+s.add("A");
+for(String i:s)
+System.out.println(i);
+}
+}
