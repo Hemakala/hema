2d6d58679efebc56ec8ae10fd1acaa7ece6dcc11
Hemakala <hemakala1995@gmail.com>
2016-11-17 15:25:26 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Array
+{
+public static void main(String args[])
+{
+ArrayList<Integer> s=new ArrayList<Integer>();
+s.add(1);
+s.add(2);
+s.add(3);
+for(Integer i:s)
+System.out.println(i);
+}
+}
