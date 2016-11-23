ccf3288e64cbe5da9133bafe99d2131293a3489d
Hemakala <hemakala1995@gmail.com>
2016-11-23 16:39:53 +0530

    Add files via upload

---
+++ StringName.java
@@ -0,0 +1,9 @@
+public class StringName
+{
+public static void main(String[] args){
+String str1="Hello world";
+String str2="Hello";
+boolean blnFound=str1.contains(str2);
+System.out.println("String contains another string?:" +blnFound);
+}
+}
